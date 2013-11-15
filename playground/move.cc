a8ca46287cfcef40313e1ad6db9cafbf91cc39ad
localghost <localghost@codeleet.com>
2013-11-15 23:15:47 +0100

    Check moving stack data.

--- playground/move.cc
+++ playground/move.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <utility>
+using namespace std;
 
 struct Foo
 {
@@ -21,10 +22,29 @@ struct Bar
     Foo foo;
 };
 
+struct Baz
+{
+    Baz() { cout << "ctor" << endl; }
+    Baz(const Baz&) { cout << "copy" << endl; }
+    Baz(Baz&&) { cout << "move" << endl; }
+};
+
+void baz(Baz b)
+{
+    cout << "baz" << endl;
+}
+
 int main()
 {
     Foo f{42, "42"};
     Bar b(f);
     print(f, "f: main()");
     print(b.foo, "b: main()");
+
+    cout << "<<<<<<>>>>>>" << endl;
+    Baz b;
+    cout << "-----" << endl;
+    baz(move(b));
+    cout << "-----" << endl;
+    baz(Baz());
 }
