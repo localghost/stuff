94fbebe76d3785749b75d74b9f0f0542de329aa0
localghost <localghost@codeleet.com>
2014-02-10 09:51:59 +0100

    Update file read benchmark

--- playground/benchmark/file_read/test_fstream_iterator.cpp
+++ playground/benchmark/file_read/test_fstream_iterator.cpp
@@ -8,28 +8,33 @@ namespace {
 const char* const TEST_FILE_PATH = "/tmp/file_read_test";
 }
 
-int main(int argc, char* argv[])
+std::chrono::microseconds test_fstream_iterator(const std::string& filename)
 {
-    std::ifstream ifs{TEST_FILE_PATH};
+    std::ifstream ifs{filename};
     if (!ifs)
-    {
-        std::cerr << "Could not open file '" << TEST_FILE_PATH
-                  << "' for reading" << std::endl;
-        return -1;
-    }
+        throw std::exception{}; // FIXME add some relevant message
+
     ifs >> std::noskipws;
     
     ifs.seekg(0, ifs.end);
-    std::size_t size = ifs.tellg();
+    std::size_t file_size = ifs.tellg();
     ifs.seekg(0, ifs.beg);
-    std::string content;
-    content.reserve(size+1);
 
-//    auto before = std::chrono::steady_clock::now();
+    std::string content;
+    content.reserve(file_size + 1); // TODO is +1 necessary?
+
+    auto start = std::chrono::steady_clock::now();
     content.insert(content.end(),
                    std::istream_iterator<char>{ifs},
                    std::istream_iterator<char>{});
-//    auto after = std::chrono::steady_clock::now();
+    auto end = std::chrono::steady_clock::now();
+
     ifs.close();
-    std::cout << content;
+
+    return (end - start);
+}
+
+int main(int argc, char* argv[])
+{
+    test_fstream_iterator(TEST_FILE_PATH);
 }
