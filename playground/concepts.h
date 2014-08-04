99c245a993f35ce67601b887b302865d6b121523
localghost <localghost@codeleet.com>
2014-08-04 09:19:39 +0200

    Header file with concept defines

---
+++ playground/concepts.h
@@ -0,0 +1,11 @@
+#ifndef CONCEPTS_H_
+#define CONCEPTS_H_
+
+#define InputIterator typename
+#define ForwardIterator typename
+#define BidirectionalIterator typename
+#define RandomAccessIterator typename
+
+#define StrictWeakOrdering typename
+
+#endif
